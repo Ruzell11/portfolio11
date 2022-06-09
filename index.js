153cbba0e4550d3a184c117bd2e04cf8a3102688
Ruzell11 <johnruzell123@gmail.com>
2022-06-09 17:39:32 +0800

    fix nav

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const toggle = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.querySelector('.items');
 const sideButton = document.getElementById('buttonside');
 const sideNavLinks = document.querySelector('.nav-side');
-const header = document.querySelectorAll('.dropdown')
+const header = document.querySelectorAll('.head')
 
 
 toggle.addEventListener('click' ,() =>{
